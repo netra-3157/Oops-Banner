fc5c2007a62d9f6ceb2fcb39623976e949a8d519
netra-3157 <netravenkat2531@gmail.com>
2026-05-06 00:03:16 +0530

    UC6: use methos for characters patterns (O, P, S)

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,53 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join(" ", "*****", "*****", "*****", "*****"),
-            String.join(" ", "*     *", "*     *", "*     *", "*     *"),
-            String.join(" ", "*     *", "*     *", "*     *", "*     *"),
-            String.join(" ", "*     *", "*****", "*****", "*****"),
-            String.join(" ", "*     *", "*", "*", "*"),
-            String.join(" ", "*     *", "*", "*", "*"),
-            String.join(" ", "*****", "*", "*****", "*****")
-        };
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        // Print OOPS (O O P S)
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
         }
+    }
+
+    // Method for O
+    public static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method for P
+    public static String[] getP() {
+        return new String[]{
+            "***** ",
+            "*    *",
+            "*    *",
+            "***** ",
+            "*     ",
+            "*     ",
+            "*     "
+        };
+    }
+
+    // Method for S
+    public static String[] getS() {
+        return new String[]{
+            " *****",
+            "*     ",
+            "*     ",
+            " *****",
+            "      *",
+            "      *",
+            " *****"
+        };
     }
 }
